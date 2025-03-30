90dc3be75eac974763f4a90e0f429b491f60b2c9
GARGEYAMITHRA <gargeyamithra18gali@gmail.com>
2025-03-30 21:50:03 +0530

    done

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-// Scroll trigger to animate skills when they are in view
+
 window.addEventListener('scroll', function () {
     const skillsSection = document.querySelector('#skills');
     const skills = document.querySelectorAll('.skill');
     const sectionTop = skillsSection.getBoundingClientRect().top;
 
-    // If the skills section is visible in the viewport, trigger the animation
     if (sectionTop < window.innerHeight) {
         skills.forEach((skill, index) => {
-            skill.style.animation = `slideIn 1s forwards ${index * 0.3}s`; // Adjusting delay dynamically
+            skill.style.animation = `slideIn 1s forwards ${index * 0.3}s`; 
         });
     }
 });
